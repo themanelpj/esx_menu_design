998b9496bb067ca5550c0a971e5ba3ffc89f07b7
themanelpj <77340003+themanelpj@users.noreply.github.com>
2021-09-27 07:59:57 +0200

    Update __resource

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'ESX Menu Default - Redesigned by themanelpj'
+description 'ESX Menu Default - Redesigned by themanelpj Credits to Jaareet'
 
 client_scripts {
 	'@es_extended/client/wrapper.lua',
